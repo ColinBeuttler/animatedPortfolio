8b20a98ef512879280fb5f9a9e58d5ee44fc7cb6
Colin <beutt102@mail.chapman.edu>
2023-06-23 11:02:50 -0700

    fixed panel popup fucntion

--- public/script.js
+++ public/script.js
@@ -1,17 +1,35 @@
 const about = document.getElementById("about")
 const bio = document.getElementById("biotext")
 
+const code = document.getElementById("code")
+const codepanel = document.getElementById("codepanel")
 
-about.onclick = function(){displayBio()}
+const other = document.getElementById("other")
+const otherpanel = document.getElementById("otherpanel")
 
-function displayBio(){
+const contact = document.getElementById("contacts")
+const contactpanel = document.getElementById("contactpanel")
 
-    if(bio.style = "display:none"){
-       bio.style = "display:block" 
+
+about.onclick = function(){displayDiv(bio)}
+code.onclick = function(){displayDiv(codepanel)}
+other.onclick = function(){displayDiv(otherpanel)}
+contact.onclick = function(){displayDiv(contactpanel)}
+
+function displayDiv(divbox){
+
+    if(divbox.style.display != "none"){
+       divbox.style = "display:none" 
     }
-  else if(bio.style = "display:block"){
-    console.log(bio.style)
-    bio.style = "display:none"
+  else{
+    console.log(divbox.style)
+    divbox.style = "display:block"
   }
     
 }
+
+
+displayDiv(contactpanel)
+displayDiv(otherpanel)
+displayDiv(codepanel)
+displayDiv(bio)
