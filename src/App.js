2565e82b844f1c5d7ad304d2048fedc5f37ffdfe
Colin <beutt102@mail.chapman.edu>
2023-08-11 17:39:06 -0700

    updated to React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/HomePage/HomePage';
+import CodePage from './Components/PanelPages/CodePage';
+import ProjectPage from './Components/PanelPages/ProjectPage';
+
+function App() {
+  return (
+    <div className="App">
+
+        <Routes>
+
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="code" element={<CodePage />} />
+          <Route exact path="projects" element={<ProjectPage />} />
+
+        </Routes>
+
+    </div>
+  );
+}
+
+export default App;
